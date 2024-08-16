6beee3395e12880a2ff3665e0c804a156028be84
AlexPham0209 <121077648+AlexPham0209@users.noreply.github.com>
2024-08-16 15:59:59 -0400

    Refactored growth code

--- src/Scripts/Amoeba.gd
+++ src/Scripts/Amoeba.gd
@@ -11,12 +11,18 @@ extends CharacterBody2D
 var growth : float
 var total : float = 0
 
+var scale_tween : Tween
+
 func _ready() -> void:
 	growth = growth_rate
 
 func _physics_process(delta: float) -> void:
 	self.move()
-	self.grow(delta)
+	
+	#Test growth
+	if Input.is_action_just_pressed("Interact"):
+		grow(0.25)
+		
 	self.move_and_slide()
 
 func move():
@@ -25,6 +31,7 @@ func move():
 	
 	var direction = Vector2(x, y)
 	
+	#Add friction and acceleration
 	if direction.length() != 0:
 		velocity.x = move_toward(velocity.x, direction.x * speed, acceleration)
 		velocity.y = move_toward(velocity.y, direction.y * speed, acceleration)
@@ -33,17 +40,24 @@ func move():
 		velocity.x = move_toward(velocity.x, 0, friction)
 		velocity.y = move_toward(velocity.y, 0, friction)
 	
-func grow(delta):
+func grow(amount):
+	#Once reached max growth, then go to the next level
 	if total >= max_growth:
 		print("Max growth.  Continue to next level")
 		return 
-		
-	if not Input.is_action_pressed("Interact"):
-		return
 	
-	total += growth
-	self.scale += Vector2(growth, growth) * delta
-	growth *= 1.01
+	#Increment total growth
+	total += amount
 	
-	camera.zoom -= Vector2(growth_rate, growth_rate) * delta
+	#Calculate new scale
+	var new_scale = self.scale + Vector2(amount, amount)
+	var new_zoom = camera.zoom - (Vector2(amount, amount) * 0.1)
+	
+	#Procedurally animate the scale of the player and the zoom of the camera to new sizes
+	scale_tween = create_tween()
+	scale_tween.parallel().tween_property(self, "scale", new_scale, 1.0). \
+		set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
+	scale_tween.parallel().tween_property(camera, "zoom", new_zoom, 1.0). \
+		set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
+	scale_tween.play()
 	
--- src/Scripts/Enemy.gd
+++ src/Scripts/Enemy.gd
@@ -2,12 +2,18 @@ class_name Enemy
 extends Node2D
 
 @export var total = 0
+@export var margin : Vector2 = Vector2(0.25, 0.25)
+var player : Amoeba = null
 
-
-func _on_eat_area_area_entered(area: Area2D) -> void:
-	var player : Amoeba = area.get_parent()
-
-	if player is not Amoeba or player.scale < self.scale + Vector2(0.25, 0.25):
-		return 
+func _process(delta: float) -> void:
+	if player != null and player.scale >= self.scale + margin:
+		queue_free()
 		
-	queue_free()
+func _on_eat_area_area_entered(area: Area2D) -> void:
+	if area.get_parent() is not Amoeba:
+		return 
+	
+	player = area.get_parent()
+
+func _on_eat_area_area_exited(area: Area2D) -> void:
+	player = null
